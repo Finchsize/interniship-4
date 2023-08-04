0626d9660e238e61bc1b92df808954b3930779f3
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-04 11:40:21 +0200

    I4-8 - `/register` returns JWT

--- backend/src/main/java/com/example/user/UserController.java
+++ backend/src/main/java/com/example/user/UserController.java
@@ -1,10 +1,18 @@
 package com.example.user;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +22,9 @@ import java.util.Optional;
 public class UserController {
 
     private final UserRepository userRepository;
+
+    // JWT algorithm
+    Algorithm algorithm = Algorithm.HMAC256("449d0065577de8cc67efc7abe049ef5514cc29a8ebc28dbd120954b24db9e797");
 
     // Find all users
     @GetMapping
@@ -29,7 +40,7 @@ public class UserController {
 
     // Register a new user
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
+    public String registerUser(@RequestBody User user) {
         // Check if user with specified name or email exists
         if (userRepository.existsByName(user.getName())) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Name is already taken");
@@ -40,7 +51,14 @@ public class UserController {
 
         // Save new user to the database
         User newUser = userRepository.save(user);
-        return newUser;
+
+        try {
+            // Create a JWT
+            String token = JWT.create().withClaim("id", newUser.getId()).withIssuedAt(new Date()).withExpiresAt(new Date(System.currentTimeMillis() + 5000L)).sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Encountered error while creating a JWT.", exception);
+        }
     }
 
     // Delete an user
