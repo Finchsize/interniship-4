2397ed590fbfa062cf2dbf925ca4c3e951dbaf53
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-04 11:59:25 +0200

    I4-8 - Remove unnecessary comments

--- backend/src/main/java/com/example/user/UserController.java
+++ backend/src/main/java/com/example/user/UserController.java
@@ -23,8 +23,7 @@ public class UserController {
 
     private final UserRepository userRepository;
 
-    // JWT algorithm
-    Algorithm algorithm = Algorithm.HMAC256("449d0065577de8cc67efc7abe049ef5514cc29a8ebc28dbd120954b24db9e797");
+    Algorithm jwtAlgorithm = Algorithm.HMAC256("449d0065577de8cc67efc7abe049ef5514cc29a8ebc28dbd120954b24db9e797");
 
     @GetMapping
     public List<User> findAll() {
@@ -36,7 +35,6 @@ public class UserController {
         return userRepository.findById(id);
     }
 
-    // Register a new user
     @PostMapping("/register")
     public String registerUser(@RequestBody User user) {
         // Check if user with specified name or email exists
@@ -46,19 +44,17 @@ public class UserController {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Email is already taken");
         }
-
-        // Save new user to the database
+        
         User newUser = userRepository.save(user);
 
         try {
-            // Create a JWT
             String token = JWT.create().withClaim("id", newUser.getId()).withIssuedAt(new Date()).withExpiresAt(new Date(System.currentTimeMillis() + 5000L)).sign(algorithm);
             return token;
         } catch (JWTCreationException exception) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Encountered error while creating a JWT.", exception);
         }
     }
-    
+
     public void deleteUser(@PathVariable int id) {
         userRepository.deleteById(id);
     }
