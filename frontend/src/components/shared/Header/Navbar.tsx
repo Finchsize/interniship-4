ee17325b0b540bd1a327b2ec7f26f9133c775009
Maksymilian <makschmielak@gmail.com>
2023-08-07 14:20:10 +0200

    I4-3 - Remove whitespace in Navbar.tsx

--- frontend/src/components/shared/Header/Navbar.tsx
+++ frontend/src/components/shared/Header/Navbar.tsx
@@ -2,7 +2,7 @@ export function Navbar(props: { names: string[] }) {
   return (
     <div className="flex items-center gap-16">
       {props.names.map((name, index) => {
-        return <p className=" text-3xl text-neutral-700"> {name}</p>;
+        return <p className=" text-3xl text-neutral-700">{name}</p>;
       })}
     </div>
   );
