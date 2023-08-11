25ad722d376f2d36e2c0a2a86f162f2fb24ece3f
Maksymilian <makschmielak@gmail.com>
2023-08-11 10:23:59 +0200

    I4-28 - Add working Login page, implement React Hook Form for Sign in page, fix dependency warning

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,68 @@
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import Cookies from "universal-cookie";
+
+type Inputs = {
+  password: string;
+};
+
 export function Login() {
+  const { nickname } = useParams();
+  const navigate = useNavigate();
+  const cookies = new Cookies();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    if (typeof formData.password != "string") {
+      console.log("Form validation error");
+    } else {
+      const payload = {
+        name: nickname,
+        password: formData.password,
+      };
+      try {
+        axios
+          .post(process.env.REACT_APP_API + "user/login", payload)
+          .then((response) => {
+            const jwtToken = response.data;
+            if (typeof jwtToken != "string") {
+              console.log(response.status);
+            } else {
+              cookies.set("jwt", jwtToken, {
+                expires: new Date(Date.now() + 5 * 60 * 1000),
+                secure: true,
+                httpOnly: true,
+                sameSite: "lax",
+              });
+              navigate("/");
+            }
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div>
       <h1>Welcome back</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Password"
+          {...register("password", {
+            required: "Password is required",
+          })}
+        />
+        <p>{errors.password?.message}</p>
+        <input type="submit" value="Log in" />
+      </form>
     </div>
   );
 }
