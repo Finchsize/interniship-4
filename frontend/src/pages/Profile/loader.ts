28e08d8767a02300ae8758ef43aded8d83b9e104
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-18 14:30:20 +0200

    I4-47 - Add task number to the comment

--- frontend/src/pages/Profile/loader.ts
+++ frontend/src/pages/Profile/loader.ts
@@ -2,7 +2,7 @@ import { Params } from "react-router-dom";
 import axios from "axios";
 
 const loader = async ({ params }: { params: Params }) => {
-  // TODO: Check the JWT, get the user from it, and get his data
+  // TODO: Check the JWT, get the user from it, and get his data [I4-52]
   // For now, use a specific user for demonstration purposes
   const data = await axios
     .get(`${process.env.REACT_APP_API}user/1`)
