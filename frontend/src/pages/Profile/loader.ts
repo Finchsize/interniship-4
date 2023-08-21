ede5bc5e5fd68c0bd4554010505a9147dc0198d0
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-21 13:45:22 +0200

    I4-52 - Provide `loader` right endpoint

--- frontend/src/pages/Profile/loader.ts
+++ frontend/src/pages/Profile/loader.ts
@@ -1,11 +1,10 @@
-import { Params } from "react-router-dom";
 import axios from "axios";
 
-const loader = async ({ params }: { params: Params }) => {
+const loader = async () => {
   // TODO: Check the JWT, get the user from it, and get his data [I4-52]
   // For now, use a specific user for demonstration purposes
   const data = await axios
-    .get(`${process.env.REACT_APP_API}user/1`)
+    .get(`${process.env.REACT_APP_API}user/current-user`)
     .then((response) => {
       return response.data;
     });
