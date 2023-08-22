aee0524eb8dafd4294446a47070b15f12e602440
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-22 14:53:59 +0200

    I4-23 - Get `id` from token

--- backend/src/main/java/com/example/player/PlayerController.java
+++ backend/src/main/java/com/example/player/PlayerController.java
@@ -1,24 +1,25 @@
 package com.example.player;
 
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.jsonwebtoken.JsonWebToken;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/player")
 @RequiredArgsConstructor
 public class PlayerController {
     private final PlayerService playerService;
+    private final JsonWebToken jsonWebToken;
     @PutMapping("/unstuck")
     public void unstuck(@CookieValue( name = "jwt" ) String token) {
-        /*
         try {
-            DecodedJWT decodedJWT = verifier.verify(token);
+            final var decodedJWT = jsonWebToken.getVerifier().verify(token);
             playerService.unstuck(decodedJWT.getClaim("id").asInt());
         } catch (JWTCreationException exception) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token", exception);
         }
-         */
     }
 }
