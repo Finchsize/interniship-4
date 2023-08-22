f8276e73f6bb663c4b10c10f4f5da68491e96554
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-22 15:05:41 +0200

    I4-23 - Remove `setter`

--- backend/src/main/java/com/example/jsonwebtoken/JsonWebToken.java
+++ backend/src/main/java/com/example/jsonwebtoken/JsonWebToken.java
@@ -5,10 +5,8 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class JsonWebToken {
     private Algorithm algorithm = Algorithm.HMAC256("449d0065577de8cc67efc7abe049ef5514cc29a8ebc28dbd120954b24db9e797");
