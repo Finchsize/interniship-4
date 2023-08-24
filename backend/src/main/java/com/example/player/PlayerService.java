06a093939910224726b9230878a12799069247b6
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-24 10:38:29 +0200

    I4-23 - Create magic values

--- backend/src/main/java/com/example/player/PlayerService.java
+++ backend/src/main/java/com/example/player/PlayerService.java
@@ -10,14 +10,19 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PlayerService {
+    private static final int spawnX = 429;
+    private static final int spawnY = 378;
+    private static final int mapId = 1002;
+
     private final PlayerRepository playerRepository;
     public void unstuck(int id) {
-        Optional<Player> player = playerRepository.findByAccountId(id);
+        final var player = playerRepository.findByAccountId(id);
         if (player.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "No player with specified ID found");
         }
-        player.get().setRecordx(429);
-        player.get().setRecordy(378);
+        player.get().setRecordx(spawnX);
+        player.get().setRecordy(spawnY);
+        player.get().setRecordMapId(1002);
         playerRepository.save(player.get());
     }
 }
