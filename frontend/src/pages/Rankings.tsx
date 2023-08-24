c6cd60a3b0638d952f3d30285b48ad516c925aa2
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-08-24 15:25:48 +0200

    I4-63 - Start creating ranking table

--- frontend/src/pages/Rankings.tsx
+++ frontend/src/pages/Rankings.tsx
@@ -1,13 +1,30 @@
 const Rankings = () => {
   return (
-    <>
+    <div className="space-y-8">
       <div className="space-y-2">
         <p className="text-2xl font-semibold">Rankings</p>
         <p className="text-lg text-neutral-700">
           See how you compare to other players.
         </p>
       </div>
-    </>
+      <p className="text-lg font-semibold">Top Nobility</p>
+      <table className="w-full table-auto text-left">
+        <thead>
+          <tr>
+            <th>Place</th>
+            <th>Name</th>
+            <th>Donations</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>bruh</td>
+            <td>135135135</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
