b7d545da26baff23b3783ffc0c4b7a7d0c9c032b
Maciej <mejsiejdev@gmail.com>
2023-08-28 11:09:37 +0200

    I4-63 - Display `data` provided to `Table`

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -3,27 +3,24 @@ const Table = ({
   data,
 }: {
   headers: string[];
-  data: Array<string[]>[];
+  data: Array<string>[];
 }) => (
   <table className="w-full table-auto text-left">
     <thead>
       <tr>
         {headers.map((header) => (
-          <th>{header}</th>
+          <th className="font-medium">{header}</th>
         ))}
       </tr>
     </thead>
     <tbody>
-      <tr className="border-b border-neutral-300 last:border-none">
-        <td className="py-4">1</td>
-        <td>bruh</td>
-        <td>135135135</td>
-      </tr>
-      <tr className="border-neutral-300">
-        <td className="py-4">1</td>
-        <td>bruh</td>
-        <td>135135135</td>
-      </tr>
+      {data.map((row) => (
+        <tr className="border-b border-neutral-300 last:border-none">
+          {row.map((cell) => (
+            <td className="py-4">{cell}</td>
+          ))}
+        </tr>
+      ))}
     </tbody>
   </table>
 );
